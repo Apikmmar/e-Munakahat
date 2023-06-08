727315ee2f12bc5380f78fb12bc5bd4d690e2da7
aimananuar2112 <89570300+aimananuar2112@users.noreply.github.com>
2023-06-08 14:55:12 +0800

    --

--- apps/m5_userviewapplication.php
+++ apps/m5_userviewapplication.php
@@ -61,7 +61,36 @@
                 <span class="h6 text-uppercase">Bantuan Insentif Khas</span>
             </div>
             <div class="content-of-module">
-                Hello
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
