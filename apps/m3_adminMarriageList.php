ef4033bdeb3e1ef0272a3bd0e51cdded8660105b
apikammar <apikammar07@gmail.com>
2023-06-11 10:41:36 +0800

    byabbythebestprogrammer

--- apps/m3_adminMarriageList.php
+++ apps/m3_adminMarriageList.php
@@ -11,7 +11,7 @@ include "connection.php";
   <title>e-Munakahat</title>
   <link rel="shortcut icon" href="img/jata-pahang.png" type="image/png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="assets/style.css">
   <link rel="stylesheet" href="assets/css/module3.css">
 
   <style>
