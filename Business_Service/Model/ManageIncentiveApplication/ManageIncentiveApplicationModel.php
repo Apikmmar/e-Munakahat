74b5f54bcd492e4bb4042d748884d34fdb3f0030
aimananuar2112 <89570300+aimananuar2112@users.noreply.github.com>
2023-06-16 12:17:36 +0800

    ..

--- Business_Service/Model/ManageIncentiveApplication/ManageIncentiveApplicationModel.php
+++ Business_Service/Model/ManageIncentiveApplication/ManageIncentiveApplicationModel.php
@@ -1,1 +1,19 @@
-<?php ?>
+<?php
+
+class IncentiveApplicationModel {
+    private $db;
+
+    public function __construct() {
+        // Create a new instance of the Database class
+        $this->db = new Database();
+    }
+
+    
+
+    public function getApplication() {
+        $query = "SELECT * FROM incentive_application";
+        return $this->db->execute($query);
+    }
+
+}
+?>
